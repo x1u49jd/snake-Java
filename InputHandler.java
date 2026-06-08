be6d2aa53f7a9cae4af1aa8dd13bded7f4adf37e
x1u49jd <jakub.dzido00@gmail.com>
2026-06-08 23:05:03 +0100

    feat: add input handler for snake direction

---
+++ InputHandler.java
@@ -0,0 +1,37 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class InputHandler implements KeyListener {
+
+    private String direction = "RIGHT";
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key =  e.getKeyCode();
+
+        if (key == KeyEvent.VK_UP) {
+            direction = "UP";
+        }
+        else if (key == KeyEvent.VK_DOWN) {
+            direction = "DOWN";
+        }
+        else if (key == KeyEvent.VK_LEFT) {
+            direction = "LEFT";
+
+        }
+        else if (key == KeyEvent.VK_RIGHT) {
+            direction = "RIGHT";
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
