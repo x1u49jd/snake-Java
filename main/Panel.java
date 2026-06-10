1c788e6e71e11308a97c3a7dd9cafc6ef2111d5f
x1u49jd <jakub.dzido00@gmail.com>
2026-06-10 21:46:21 +0100

    feat: implement update method inside Panel class to refresh cell states based on snake position

--- main/Panel.java
+++ main/Panel.java
@@ -3,11 +3,12 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
+import entities.Snake;
+
 public class Panel extends JPanel {
     private final int rows = 20;
     private final int cols = 20;
     private final int cellSize = 25;
-
     private Cell[][] cells;
 
     public Panel() {
@@ -21,11 +22,6 @@ public class Panel extends JPanel {
                 cells[r][c] = new Cell(r, c, cellSize);
             }
         }
-
-        cells[5][5].setActive(true);
-        cells[5][6].setActive(true);
-        cells[5][7].setActive(true);
-        cells[6][8].setActive(true);
     }
 
     @Override
@@ -38,6 +34,26 @@ public class Panel extends JPanel {
                 cells[r][c].draw(g);
             }
         }
+    }
+
+    public void update(Snake snake) {
+        // set all cells in the grid to false (gray)
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                cells[r][c].setActive(false);
+            }
+        }
+
+        // for every segment of the snake, 
+        // find the matching cell in the grid and mark it as true (green)
+        for (int i = 0; i < snake.getBody().size(); i++) {
+            int[] pos = snake.getBody().get(i);
+            int r = pos[0];
+            int c = pos[1];
+            cells[r][c].setActive(true);
+        }
+
+        repaint();
     }
 
 }
