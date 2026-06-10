f7534d6bb8896fa6c83af8fe82eb6526afd6322e
x1u49jd <jakub.dzido00@gmail.com>
2026-06-10 21:54:46 +0100

    feat: initialise objects at the top and implement game loop

--- main/Game.java
+++ main/Game.java
@@ -1,11 +1,30 @@
 package main;
 import javax.swing.JFrame;
 
+import entities.Snake;
+
 public class Game {
+    JFrame frame = new JFrame();
+    Panel panel = new Panel();
+    InputHandler inputHandler = new InputHandler();
+    Snake snake = new Snake(5, 5);
+
+    public void gameLoop() {
+        while(true) {
+            snake.move(inputHandler.getDirection());
+            panel.update(snake);
+
+            // wait 120 ms between updates (roughly 8 fps)
+            try {
+                Thread.sleep(120);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public Game() {
-        JFrame frame = new JFrame();
-        Panel panel = new Panel();
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
@@ -13,5 +32,8 @@ public class Game {
         frame.setTitle("Snake");
         frame.pack();
         frame.setLocationRelativeTo(null);
+        frame.addKeyListener(inputHandler);
+
+        new Thread(this::gameLoop).start();
     }
 }
